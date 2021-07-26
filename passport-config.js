da1322ca9424d303333d8ae40482bf7f2ed8ae77
Mansi-kumari <70839625+Mansi-kumari@users.noreply.github.com>
2021-07-26 16:50:26 +0530

    Add files via upload

---
+++ passport-config.js
@@ -0,0 +1,29 @@
+const LocalStrategy =require('passport-local').Strategy
+const bcrypy = required('bcrypt')
+
+ function initialize(Passport,getUserByEmail,getUserById){
+    const authenticateUser = async(email,password,done) => {
+    const user = getUserByEmail(email)
+    if (user == null)
+    {
+        return done(null,false,{message : 'No user found'})
+    }
+    try{
+        if(await bcrypt.compare(password,user.password))
+        {
+            return done(null,user)
+        } else{
+            return done(null,false,{message : 'incorrect password'})
+        }
+    }catch (e){
+        return done(e)
+    }
+    }
+    passport.use(new LocalStrategy({ usernameField : 'email'}, authenticateUser))
+    passport.serializeUser((user,done)=> done(null,user.id))
+    passport.serializeUser((id,done)=> {
+       return done(null,getUserById(id))
+    }) 
+}
+
+module.exports = initialize
